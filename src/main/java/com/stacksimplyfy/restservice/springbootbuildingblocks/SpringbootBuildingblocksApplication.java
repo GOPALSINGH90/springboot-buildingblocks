327f36838c8a50b9232ce36a90b5a36c69476e7e
Gopal Singh <gopal.singh@nineleaps.com>
2020-04-19 01:18:04 +0530

    first commit

---
+++ src/main/java/com/stacksimplyfy/restservice/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -0,0 +1,14 @@
+package com.stacksimplyfy.restservice.springbootbuildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
